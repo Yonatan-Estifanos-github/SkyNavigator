5adc9b88cc2cfc4d6ace0e8fc56270d107d87b4a
Yonatan Estifanos <yonatanestifanos58850@gmail.com>
2024-05-27 21:56:15 -0400

    Update SatellitesController with improved data handling

--- SkyNavigatorBackend/Controller/SatellitesController.cs
+++ SkyNavigatorBackend/Controller/SatellitesController.cs
@@ -10,10 +10,11 @@ namespace SkyNavigatorBackend.Controllers
         [HttpGet]
         public IEnumerable<Satellite> Get()
         {
+            // Mock data for testing
             return new List<Satellite>
             {
-                new Satellite { Id = 1, Name = "Hubble Space Telescope" },
-                new Satellite { Id = 2, Name = "International Space Station" }
+                new Satellite { Id = 1, Name = "Hubble Space Telescope", Latitude = 28.5, Longitude = -80.6 },
+                new Satellite { Id = 2, Name = "International Space Station", Latitude = 51.6, Longitude = -0.1 }
             };
         }
     }
@@ -22,5 +23,7 @@ namespace SkyNavigatorBackend.Controllers
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
     }
 }
