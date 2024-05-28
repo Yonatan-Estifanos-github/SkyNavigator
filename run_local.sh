ff50b1df80ccb0e8ecec7cac9a10ba2a8fde03e7
Yonatan Estifanos <yonatanestifanos58850@gmail.com>
2024-05-27 21:56:17 -0400

    Add run_local.sh script for local development

--- run_local.sh
+++ run_local.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-# Function to open a new terminal and run a command
-run_in_new_terminal() {
-  local cmd=$1
-  osascript <<END
-  tell application "Terminal"
-    do script "$cmd"
-  end tell
-END
-}
-
-# Start backend
-run_in_new_terminal "cd $(pwd)/SkyNavigatorBackend && dotnet run"
-
-# Start frontend
-run_in_new_terminal "cd $(pwd)/sky-navigator && npm start"
+# Run both frontend and backend concurrently
+concurrently --kill-others-on-fail \
+  "cd SkyNavigatorBackend && dotnet run" \
+  "cd sky-navigator && npm start"
