8056aac1f845a0c8519fd56dabc3271dd0a4a63b
Yonatan Estifanos <yonatanestifanos58850@gmail.com>
2024-05-27 21:56:17 -0400

    Update api.js to handle new backend endpoints

--- sky-navigator/services/api.js
+++ sky-navigator/services/api.js
@@ -1,17 +1,13 @@
-// sky-navigator/src/services/api.js
-
-// Example API call function
 export const fetchSatellites = async () => {
-    try {
-      const response = await fetch('http://localhost:5001/api/satellites'); // Update the endpoint as needed
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error('Fetch error:', error);
-      throw error;
+  try {
+    const response = await fetch('http://localhost:5001/api/satellites');
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
     }
-  };
-  
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Fetch error:', error);
+    throw error;
+  }
+};
